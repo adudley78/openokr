f40d9c62f72e6e8701294f58e7366a25566dcde9
adudley78 <dudleyadam@gmail.com>
2019-11-29 12:09:24 -0500

    Remove measure attribute

--- spec/factories/key_results.rb
+++ spec/factories/key_results.rb
@@ -3,10 +3,8 @@ require 'faker'
 FactoryBot.define do
   factory :key_result do
     description { Faker::Lorem.word }
-    # TODO: rename or delete measure of success from the model
-    measure { Faker::Number.decimal(digits: 1) }
     due_date { Faker::Date }
-    objective_id { Faker::Number.number(digits: 1) }
-    user_id { Faker::Number.number(digits: 1) }
+    # objective_id { Faker::Number.number(digits: 1) }
+    # user_id { Faker::Number.number(digits: 1) }
   end
 end
