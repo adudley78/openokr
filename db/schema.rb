763d5e3774a223b214f4390543b53bbc5434c21f
adudley78 <dudleyadam@gmail.com>
2019-11-29 12:09:53 -0500

    Remove measure attribute from Key Results

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_175110) do
+ActiveRecord::Schema.define(version: 2019_11_29_170539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "key_results", force: :cascade do |t|
     t.text "description"
-    t.string "measure"
     t.string "due_date"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
